1a55ade4f6ed10924a8a5ce5649ef41fd5f42955
sumana.mahata <sumanam.jgi@gmail.com>
2026-09-20 14:47:08 +0530

    chore: initialize Spring Boot backend

---
+++ backend/src/test/java/com/smilley/finance/FinanceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smilley.finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
